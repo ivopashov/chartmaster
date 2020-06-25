37f88fb7051d155aeb125500695cd0692035f94d
Ivaylo Pashov <ivo.pashoff@gmail.com>
2020-06-25 16:20:57 +0300

    Updated schema rb and structure sql

--- db/schema.rb
+++ db/schema.rb
@@ -1,183 +1,40 @@
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-SET default_tablespace = '';
+ActiveRecord::Schema.define(version: 2) do
 
-SET default_table_access_method = heap;
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
---
--- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
---
+  create_table "stock_snapshots", force: :cascade do |t|
+    t.string "ticker", null: false
+    t.float "close", default: 0.0
+    t.float "volume", default: 0.0
+    t.float "open", default: 0.0
+    t.float "high", default: 0.0
+    t.float "low", default: 0.0
+    t.date "date", null: false
+    t.float "sma20", default: 0.0
+    t.float "sma50", default: 0.0
+    t.float "sma200", default: 0.0
+    t.float "rsi", default: 0.0
+    t.index ["ticker", "date"], name: "index_stock_snapshots_on_ticker_and_date", unique: true
+  end
 
-CREATE TABLE public.ar_internal_metadata (
-    key character varying NOT NULL,
-    value character varying,
-    created_at timestamp(6) without time zone NOT NULL,
-    updated_at timestamp(6) without time zone NOT NULL
-);
+  create_table "symbol_maps", force: :cascade do |t|
+    t.string "ticker", null: false
+    t.string "anonymous_ticker", null: false
+    t.index ["anonymous_ticker"], name: "index_symbol_maps_on_anonymous_ticker", unique: true
+    t.index ["ticker"], name: "index_symbol_maps_on_ticker", unique: true
+  end
 
-
---
--- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.schema_migrations (
-    version character varying NOT NULL
-);
-
-
---
--- Name: stock_snapshots; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.stock_snapshots (
-    id bigint NOT NULL,
-    ticker character varying NOT NULL,
-    close double precision DEFAULT 0.0,
-    volume double precision DEFAULT 0.0,
-    open double precision DEFAULT 0.0,
-    high double precision DEFAULT 0.0,
-    low double precision DEFAULT 0.0,
-    date date NOT NULL,
-    sma20 double precision DEFAULT 0.0,
-    sma50 double precision DEFAULT 0.0,
-    sma200 double precision DEFAULT 0.0,
-    rsi double precision DEFAULT 0.0
-);
-
-
---
--- Name: stock_snapshots_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.stock_snapshots_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: stock_snapshots_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.stock_snapshots_id_seq OWNED BY public.stock_snapshots.id;
-
-
---
--- Name: symbol_maps; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.symbol_maps (
-    id bigint NOT NULL,
-    ticker character varying NOT NULL,
-    anonymous_ticker character varying NOT NULL
-);
-
-
---
--- Name: symbol_maps_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.symbol_maps_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: symbol_maps_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.symbol_maps_id_seq OWNED BY public.symbol_maps.id;
-
-
---
--- Name: stock_snapshots id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.stock_snapshots ALTER COLUMN id SET DEFAULT nextval('public.stock_snapshots_id_seq'::regclass);
-
-
---
--- Name: symbol_maps id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.symbol_maps ALTER COLUMN id SET DEFAULT nextval('public.symbol_maps_id_seq'::regclass);
-
-
---
--- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.ar_internal_metadata
-    ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
-
-
---
--- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.schema_migrations
-    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
-
-
---
--- Name: stock_snapshots stock_snapshots_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.stock_snapshots
-    ADD CONSTRAINT stock_snapshots_pkey PRIMARY KEY (id);
-
-
---
--- Name: symbol_maps symbol_maps_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.symbol_maps
-    ADD CONSTRAINT symbol_maps_pkey PRIMARY KEY (id);
-
-
---
--- Name: index_stock_snapshots_on_ticker_and_date; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE UNIQUE INDEX index_stock_snapshots_on_ticker_and_date ON public.stock_snapshots USING btree (ticker, date);
-
-
---
--- Name: index_symbol_maps_on_anonymous_ticker; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE UNIQUE INDEX index_symbol_maps_on_anonymous_ticker ON public.symbol_maps USING btree (anonymous_ticker);
-
-
---
--- Name: index_symbol_maps_on_ticker; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE UNIQUE INDEX index_symbol_maps_on_ticker ON public.symbol_maps USING btree (ticker);
-
-
---
--- PostgreSQL database dump complete
---
-
-SET search_path TO "$user", public;
-
-INSERT INTO "schema_migrations" (version) VALUES
-('1'),
-('2');
-
-
+end
